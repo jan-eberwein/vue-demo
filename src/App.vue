16ec5f04226283d63e9a616752e4741a94bd71eb
Jan Eberwein <S2410629003@fhooe.at>
2024-11-25 11:11:22 +0100

    add boilerplate code from create vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <p>2</p>
+  </main>
+</template>
+
+<style scpoed></style>
